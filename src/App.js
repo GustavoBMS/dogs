821f73e5df789e2a65b6418db2908c8497d6be2b
Gustavo Brito <gustavobrito.dll@gmail.com>
2021-10-17 19:46:19 -0300

    Projeto funcionando

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Login from './components/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      Salve
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} exact />
+          <Route path="/login/*" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/Login/LoginPasswordReset.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function LoginPasswordReset() {
+  return (
+    <div> Login Password Reset</div>
+  );
+}
+
+export default LoginPasswordReset;
